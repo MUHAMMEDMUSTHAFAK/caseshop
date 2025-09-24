451397e49f7c571f9d576a1fe707bb9cb9591be9
MUHAMMED MUSTHAFA <muhammedmusthafakt17@gmail.com>
2025-09-24 17:49:01 +0530

    Add Footer and modify Navbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,8 @@ function Footer() {
     <footer className="bg-dark text-white py-4 mt-5">
       <div className="container text-center">
         <p className="mb-1">&copy; 2025 covéra. All rights reserved.</p>
-        <p className="small">Follow us on 
-          <a href="#" className="text-white ms-2">Instagram</a>, 
-          <a href="#" className="text-white ms-2">Facebook</a>
-        </p>
+        
+         
       </div>
     </footer>
   );
